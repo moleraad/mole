9cad6b474d89e4de8d1b44bc655fc8a91105d1c5
molera <164996736+moleraad@users.noreply.github.com>
2024-05-28 14:50:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-f0047412-c911-48c8-af7d-d15a907d288c/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-c7523ab7-1a30-4684-a705-4286ffd3d30b/)
 
 
 echo "$curl_output" >> Update.tsx
